fbde401fb12f11b3e134fa99f84f209995967e6c
WALIU-ADENIJI <61590345+WALIU-ADENIJI@users.noreply.github.com>
2020-03-26 16:52:36 +0100

    adding javascript task

---
+++ First Web Page/script.js
@@ -0,0 +1,13 @@
+var name = 'waliu';
+console.log(name);	
+
+var course =  new Array('html', 'css', 'javascript', 'node.js');
+	for (i=0;i<course.length;i++){
+		console.log(course[i]);
+	}
+
+for (i=1;i<=200;i++){
+			if (i%2==0){
+				console.log(i);
+			}
+		}
